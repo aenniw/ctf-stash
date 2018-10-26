47a69eade370a7d88e6bb68fbdee3901a90e82e4
fabrymarko <fabrymarko@gmail.com>
2018-10-26 16:26:59 -0400

    moar flags

--- solves/round2/the_eventlog/pamela_decipher.py
+++ solves/round2/the_eventlog/pamela_decipher.py
@@ -1,4 +1,8 @@
-file_secret = open("pamela_secret.txt", "r")
+#!/usr/bin/env python
+
+import base64
+
+file_secret = open("message_from_chunks_plaintext_ciphered.txt", "r")
 secret = file_secret.read()
 
 # print secret
@@ -9,8 +13,8 @@ key_arr = key.split('\n')
 
 for k in key_arr:
     ki = k.split(' ')
-    secret = secret.replace(ki[0], ki[1]+"\n")
+    secret = secret.replace(ki[0], chr(int(ki[1])))
     # print ki[0]+" "+ki[1]
     # print secret
 
-print secret
+print base64.b64decode(secret)
