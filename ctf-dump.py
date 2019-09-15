37999bc0af789148ac46fb23153291124f181776
Martin Mihálek <aenniw@gmail.com>
2019-09-15 16:24:35 +0200

    Fix issue with ctf-dump.py
    - directory tree now consist only of ASCII chars
    - skip non revealed hints
    - add more robust Markdown template
    - strip query from file names
    
    Signed-off-by: Martin Mihálek <aenniw@gmail.com>

--- ctf-dump.py
+++ ctf-dump.py
@@ -3,11 +3,32 @@
 
 from requests import session
 from bs4 import BeautifulSoup
+import unidecode
 import json
 import os
 import requests
 import re
 import sys
+
+md_appendix = '''
+
+---
+
+#### Solution:
+
+```bash
+```
+
+---
+
+<details><summary>FLAG:</summary>
+
+```
+
+```
+
+</details>
+'''
 
 
 class CTFdCrawl:
@@ -18,7 +39,9 @@ class CTFdCrawl:
         self.keys = 'data'
         self.url = url
         self.ch_url = self.url + '/api/v1/challenges'
-        self.headers = {"User-Agent": "curl/7.47.0"}
+        self.headers = {
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36"
+        }
 
         if not self.login():
             raise Exception('Login Failed')
@@ -35,7 +58,7 @@ class CTFdCrawl:
         nonce = nonce.get('value')
 
         self.auth['nonce'] = nonce
-        self.title = soup.title.string
+        self.title = soup.title.string.replace(' ', '_')
 
         resp = self.ses.post(self.url + '/login', data=self.auth)
         return 'incorrect' not in resp.text
@@ -43,11 +66,6 @@ class CTFdCrawl:
     def checkVersion(self):
         resp = self.ses.get(self.ch_url, headers=self.headers)
         self.version = 'v.1.2.0' if '404' not in resp.text else 'v.1.0'
-
-    def antiCloudflare(self, page):
-        scrape = create_scraper()
-        tokens = get_tokens('{}/{}'.format(self.URL, page))
-        return tokens
 
     def parseChall(self, id):
         resp = self.ses.get('{}/{}'.format(self.ch_url, id),
@@ -99,30 +117,32 @@ class CTFdCrawl:
 
         r = re.compile("[^A-Za-z0-9 .\'_-]")
         for key, val in self.entry.items():
+            key = unidecode.unidecode(key)
             for keys, vals in val.items():
-                keys = r.sub('', keys.strip())
+                keys = r.sub('', unidecode.unidecode(keys).strip())
                 directory = '{}/{} [{} pts]'.format(key, keys, vals['Points'])
-                directory = directory.replace(' / ', '-')
+                directory = directory.replace(' / ', '-').replace(' ', '_')
                 print('Directory', directory, 'has been created')
                 if not os.path.exists(directory):
                     os.makedirs(directory)
 
                 files = vals['Files']
-                hint = vals['Hint']
+                hint = [h for h in vals['Hint'] if isinstance(h, str)]
 
                 with open('{}/README.md'.format(directory), 'w') as f:
-                    f.write('Challenge:\n')
+                    f.write('#### Challenge:\n\n')
                     f.write(vals['Description'].strip())
                     if len(hint) > 0:
-                        f.write('\n\nHints:\n{}'.format(''.join(hint)))
+                        f.write('\n\n##### Hints:\n{}'.format(', '.join(hint)))
                     if len(files) > 0:
-                        f.write('\n\nFiles:\n{}'.format(", ".join(
+                        f.write('\n\n##### Files:\n{}'.format(", ".join(
                             map(lambda f: '[{}](./{})'.format(f, f),
-                                map(lambda f: f.split('/')[-1], files)))))
+                                map(lambda f: f.split('/')[-1].split('?')[0], files)))))
+                    f.write(md_appendix)
 
                 if files:
                     for i in files:
-                        filename = i.split('/')[-1]
+                        filename = i.split('/')[-1].split('?')[0]
                         print(filename, i)
                         if not os.path.exists(directory + '/' + filename):
                             resp = self.ses.get(self.url + i,
