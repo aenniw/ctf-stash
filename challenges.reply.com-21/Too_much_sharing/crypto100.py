8bc7f3ad728afea3ee8aaeea222c8b286b0aa5d5
Marko Fabry <fabrymarko@gmail.com>
2021-12-30 18:03:54 +0100

    Add challenges.reply.com-21 - Too_much_sharing writeup

--- challenges.reply.com-21/Too_much_sharing/crypto100.py
+++ challenges.reply.com-21/Too_much_sharing/crypto100.py
@@ -1,28 +1,3 @@
-#### Challenge:
-
-Mixantula is the Legend of Tech. From the Temple of Nebula she watches over her companions and sends them support. For this, she needs your help!
-
-Mixantula is now facing a new communication problem with her companions, especially Claire. To restore all communications with her, she needs to intercept Edgard, one of the enemies, who knows part of the code to restore the connections.
-
-```
-    ...
-    Edgard = int(str(Portia)[0:32])+int(str(Quincy)[0:32])
-    while not isPrime(Edgard):
-        Edgard += 1
-    ...
-```
-
-Help our Hero find out where Edgard and his henchmen, Nathan and Philip, are hiding Portia and Quincy, who hold the key to interpreting what Claire says.
-
-[crypto100-readme.txt](./crypto100-readme.txt ":ignore")
-
----
-
-#### Solution:
-
-This challenge is classic RSA with known public modulus `N`, totient function value `Phi` and code that shows how to get public exponent `E` from `P` and `Q`. First we use the fact that from `N = P * Q` and `Phi = (P-1) * (Q-1)` we can get the values for `P` and `Q`. Using the provided code snippet it is trivial to get public exponent `E`. Since `D * E mod Phi = 1` we can easily compute private key `D` by modular inverse. Really the hardest parts of this challenge are the high school math of the system of two equations with two unknown variables and conversion of the decimal number to ASCII string.
-
-```python
 import gmpy2
 from gmpy2 import mpz
 
@@ -96,15 +71,3 @@ def convert_decimal_num_to_ascii_str(decimal_number):
     return ascii_string 
 
 print(convert_decimal_num_to_ascii_str(plain))
-
-```
-
----
-
-<details><summary>FLAG:</summary>
-
-```
-{FLG:Cha0s_h1d3s_b3h1nd_th3_m3n4c1ng_f4c3_0f_0rd3r!}
-```
-
-</details>
